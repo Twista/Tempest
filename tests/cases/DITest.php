040fbd8123e0b9beac203631a92e84d31b47b0ab
Michal Haták <me@twista.cz>
2012-10-04 20:54:38 +0200

    di test (wrong namespace)

--- tests/cases/DITest.php
+++ tests/cases/DITest.php
@@ -5,7 +5,7 @@ class DITest extends PHPUnit_Framework_TestCase{
 	private $di;
 
 	public function setUp(){
-		$this->di = new \Tempest\DI();
+		$this->di = new \Tempest\DI\Container();
 	}
 
 	public function testGetSetServices(){
@@ -17,7 +17,7 @@ class DITest extends PHPUnit_Framework_TestCase{
 		$this->di->set('Test2', function (){
 			return new StdClass();
 		});
-		$this->assertInstanceOf('Closure',$this->di->get('Test2'),'Test 2');
+		$this->assertInstanceOf('stdClass',$this->di->get('Test2'),'Test 2 - handling closure object');
 
 		$this->di->set('Test3','stdClass');
 		$this->assertInstanceOf('stdClass',$this->di->get('Test3'),'Test 3');
